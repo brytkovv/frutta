fd6e7512c20e8dcef87b1ad091e31e9a0f7b1969
Slava Brytkov <awak33n@gmail.com>
2025-02-19 13:42:13 +0300

    Change: greeting text, kb colors

--- app/utils/keyboards.py
+++ app/utils/keyboards.py
@@ -7,16 +7,17 @@ def main_menu_keyboard():
     Главное меню (stock, delivery, presents, desserts, working_hours, feedback).
     """
     kb = Keyboard(inline=True)
-    kb.add(Text(get_locale("button.home_page.stock")))
+    kb.add(Text(get_locale("button.home_page.stock")), color=KeyboardButtonColor.PRIMARY)
     kb.row()
-    kb.add(Text(get_locale("button.home_page.presents")))
+    kb.add(Text(get_locale("button.home_page.presents")), color=KeyboardButtonColor.PRIMARY)
     kb.row()
-    kb.add(Text(get_locale("button.home_page.delivery")))
-    kb.add(Text(get_locale("button.home_page.desserts")))
+    kb.add(Text(get_locale("button.home_page.delivery")), color=KeyboardButtonColor.PRIMARY)
     kb.row()
-    kb.add(Text(get_locale("button.home_page.working_hours")))
+    kb.add(Text(get_locale("button.home_page.desserts")), color=KeyboardButtonColor.PRIMARY)
     kb.row()
-    kb.add(Text(get_locale("button.home_page.feedback")))
+    kb.add(Text(get_locale("button.home_page.working_hours")), color=KeyboardButtonColor.PRIMARY)
+    kb.row()
+    kb.add(Text(get_locale("button.home_page.feedback")), color=KeyboardButtonColor.PRIMARY)
 
     return kb
 
@@ -28,17 +29,17 @@ def presents_keyboard():
     kb.add(OpenLink(
         label=get_locale("button.presents.catalog"),
         link=f"https://vk.com/market-{GROUP_ID}"
-    ))
+    ), color=KeyboardButtonColor.PRIMARY)
     kb.row()
     kb.add(OpenLink(
         label=get_locale("button.presents.call"),
         link=f"tel:+{MANAGER_PHONE}"
-    ))
+    ), color=KeyboardButtonColor.PRIMARY)
     kb.row()
     kb.add(OpenLink(
         label=get_locale("button.presents.direct_message"),
         link=f"https://vk.com/im?sel={MANAGER_ID}"
-    ))
+    ), color=KeyboardButtonColor.PRIMARY)
     kb.row()
     kb.add(Text(get_locale("button.back")))  # "назад"
     return kb
@@ -48,7 +49,7 @@ def back_menu_keyboard():
     Одна кнопка "назад". Можно использовать в любом месте, где нам нужно вернуться в главное меню.
     """
     kb = Keyboard(inline=True)
-    kb.add(Text(get_locale("button.back")))
+    kb.add(Text(get_locale("button.back")), color=KeyboardButtonColor.PRIMARY)
     return kb
 
 def admin_keyboard(descriptions):
@@ -60,7 +61,7 @@ def admin_keyboard(descriptions):
     for desc in descriptions:
         kb.add(Text(desc))
         kb.row()
-    kb.add(Text(get_locale("button.back")))
+    kb.add(Text(get_locale("button.back")), color=KeyboardButtonColor.PRIMARY)
     return kb
 
 def confirm_decline_keyboard():
